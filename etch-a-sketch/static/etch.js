946ea66b186daf49579afb0fbcd4eb5cf8de4195
BMBenkyou <178959326+BMBenkyou@users.noreply.github.com>
2025-01-23 12:48:58 +0800

    Set a var for dynamic row wrao

--- etch-a-sketch/static/etch.js
+++ etch-a-sketch/static/etch.js
@@ -1,30 +1,66 @@
-
-
+const root = document.documentElement;
 let container = document.querySelector(".container");
 const createNewGridBtn = document.querySelector(".new-grid");
+const colorBtn = document.querySelector(".color-grid");
+const clearBtn = document.querySelector(".clear-grid");
+let intGrid=0;
+let gridItems;
+clearBtn.addEventListener("click",() => updateColor("white"));
 
-
-createNewGridBtn.addEventListener("click",() => {
+createNewGridBtn.addEventListener("click",() =>{
   let gridValue = prompt("Enter the new grid size");
-  const intGrid = parseInt(gridValue,10);
+  intGrid = parseInt(gridValue,10);
   console.log(intGrid);
-  
+  updateGrid(intGrid);
 });
-//takes in the value of the grid (doesnt differentiate the rows and columns)
-for (let rows=0; rows<20; rows++){
-    for(let columns=0; columns<20; columns++){
-        let div = document.createElement("div");
-        //add a `grid-item` class to the div
-        div.classList.add("grid-item");
-        container.appendChild(div);
+
+function updateGrid(gridVal=16){
+  newGrid=gridVal;
+  container.innerHTML="";
+  console.log(newGrid);
+  root.style.setProperty('--grid-size',newGrid);
+  let totalItems = newGrid * newGrid;
+
+    for (let i = 0; i < totalItems; i++) {
+    let div = document.createElement("div");
+    div.classList.add("grid-item");
+    container.appendChild(div);
+  }
+  gridItems = document.querySelectorAll(".grid-item");
+
+  addMouseListener();
+
+} 
+
+
+function addMouseListener() {
+  gridItems.forEach(item => {
+    item.addEventListener("mouseover", () => {
+      item.style.backgroundColor = "red";
+    });
+  });
+}
+
+// Function to update the grid items' color
+function updateColor(color = "") {
+  gridItems.forEach(item => {
+    if (color == "red") {
+      item.addEventListener("mouseover", () => {
+        item.style.backgroundColor = color;
+      });
+    } else {
+      // Clear the color immediately
+      item.style.backgroundColor = color;
     }
-
+  });
 }
-document.getElementsByClassName(".grid-item").onmouseover = function() {mouseOver()};
 
-function mouseOver() {
-  document.getElementsByClassName(".grid-item").style.background= "red";
-}
+updateGrid();
+
+
+
+
+
 
 
 
